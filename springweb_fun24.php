32d418e6f1186f307f92e9731b1ef8627b745e5f
jack.yang <jack.yang@weidiannengyuangufenyouxiangongside-iMac.local>
2022-04-22 17:46:18 +0800

    jack_v1.04

--- springweb_fun24.php
+++ springweb_fun24.php
@@ -1,8 +1,40 @@
 <?php
-require_once("_inc.php");
-require_once("./include/_function.php");
-require_once("./include/_top.php");
-require_once("./include/_sidebar.php");
+    /*****************************************/
+    //檔案名稱：springweb_fun24.php
+    //後台對應位置：春天網站系統/活動表下載
+    //改版日期：2022.4.23
+    //改版設計人員：Jack
+    //改版程式人員：Jack
+    /*****************************************/
+
+    require_once("_inc.php");
+    require_once("./include/_function.php");
+    require_once("./include/_top.php");
+    require_once("./include/_sidebar_spring.php");
+
+    //程式開始 *****
+    if ($_SESSION["MM_Username"] == "") {
+        call_alert("請重新登入。", "login.php", 0);
+    }
+
+    if($_SESSION["MM_UserAuthorization"] != "admin" && $_SESSION["funtourpm"] != "1"){
+        call_alert("您沒有查看此頁的權限。", "login.php", 0);
+    }
+
+    // 刪除(有刪除圖檔功能待測)
+    if($_REQUEST["st"] == "del"){
+        $SQL = "select d2 from webdata where auton=".SqlFilter($_REQUEST["an"],"int")." and types='actiondownload'";
+        $rs = $SPConn->prepare($SQL);
+        $rs->execute();
+        $result = $rs->fetch(PDO::FETCH_ASSOC);
+        if($result){
+            DelFile("upload_image".$result["d2"]);
+            $SQL = "UPDATE webdata SET d2=NULL where auton=".SqlFilter($_REQUEST["an"],"int")." and types='actiondownload'";
+            $rs = $SPConn->prepare($SQL);
+            $rs->execute();
+        }
+        reURL("springweb_fun24.php");
+    }
 ?>
 
 <!-- MIDDLE -->
@@ -34,86 +66,34 @@ require_once("./include/_sidebar.php");
                             <th>圖片</th>
                             <th>操作</th>
                         </tr>
-
-                        <tr>
-                            <td>台中</td>
-                            <td>
-
-                                <a href="upload_image/event_442.jpg" class="fancybox"><img src="upload_image/event_442.jpg" border=0 height=40></a>
-
-                            </td>
-                            <td>
-                                <a href="javascript:Mars_popup('springweb_fun24_add.php?an=442','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
-                                <a title="刪除" href="springweb_fun24.php?st=del&an=442">刪除</a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>台北</td>
-                            <td>
-
-                                <a href="upload_image/event_439.jpg" class="fancybox"><img src="upload_image/event_439.jpg" border=0 height=40></a>
-
-                            </td>
-                            <td>
-                                <a href="javascript:Mars_popup('springweb_fun24_add.php?an=439','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
-                                <a title="刪除" href="springweb_fun24.php?st=del&an=439">刪除</a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>台南</td>
-                            <td>
-
-                                <a href="upload_image/event_443.jpg" class="fancybox"><img src="upload_image/event_443.jpg" border=0 height=40></a>
-
-                            </td>
-                            <td>
-                                <a href="javascript:Mars_popup('springweb_fun24_add.php?an=443','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
-                                <a title="刪除" href="springweb_fun24.php?st=del&an=443">刪除</a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>桃園</td>
-                            <td>
-
-                                <a href="upload_image/event_440.jpg" class="fancybox"><img src="upload_image/event_440.jpg" border=0 height=40></a>
-
-                            </td>
-                            <td>
-                                <a href="javascript:Mars_popup('springweb_fun24_add.php?an=440','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
-                                <a title="刪除" href="springweb_fun24.php?st=del&an=440">刪除</a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>高雄</td>
-                            <td>
-
-                                <a href="upload_image/event_444.jpg" class="fancybox"><img src="upload_image/event_444.jpg" border=0 height=40></a>
-
-                            </td>
-                            <td>
-                                <a href="javascript:Mars_popup('springweb_fun24_add.php?an=444','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
-                                <a title="刪除" href="springweb_fun24.php?st=del&an=444">刪除</a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>新竹</td>
-                            <td>
-
-                                <a href="upload_image/event_441.jpg" class="fancybox"><img src="upload_image/event_441.jpg" border=0 height=40></a>
-
-                            </td>
-                            <td>
-                                <a href="javascript:Mars_popup('springweb_fun24_add.php?an=441','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
-                                <a title="刪除" href="springweb_fun24.php?st=del&an=441">刪除</a>
-                            </td>
-                        </tr>
-
-
+                        <?php 
+                            $SQL = "SELECT * FROM webdata where types='actiondownload' order by d1 asc";
+                            $rs = $SPConn->prepare($SQL);
+                            $rs->execute();
+                            $result = $rs->fetch(PDO::FETCH_ASSOC);
+                            if($result){
+                                foreach($result as $re){ ?>
+                                    <tr>
+                                        <td><?php echo $re["d1"]; ?><%=rs("d1")%></td>				
+                                        <td>
+                                            <?php 
+                                                if($re["d2"] != ""){ ?>
+                                                     <a href="upload_image/<?php echo $re["d2"]; ?>" class="fancybox"><img src="upload_image/<?php echo $re["d2"]; ?>" border=0 height=40></a>
+                                                <?php }else{
+                                                    echo "無";
+                                                }
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <a href="javascript:Mars_popup('springweb_fun24_add.asp?an=<%=rs("auton")%>','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=690,height=300,top=10,left=10');">編輯</a>
+                                            <a title="刪除" href="springweb_fun24.asp?st=del&an=<%=rs("auton")%>">刪除</a>						
+                                        </td>
+                                    </tr>
+                                <?php }
+                            }else{
+                                echo "<tr><td colspan=4>目前無資料</td></tr>";
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
--- springweb_fun24_add.php
+++ springweb_fun24_add.php
@@ -1,23 +1,101 @@
-<meta charset="utf-8" />
+<?php 
+    /*****************************************/
+    //檔案名稱：springweb_fun24_add.php
+    //後台對應位置：春天網站系統/活動表下載/修改活動表
+    //改版日期：2022.4.23
+    //改版設計人員：Jack
+    //改版程式人員：Jack
+    /*****************************************/
 
-<script src="js/jquery-1.8.3.js"></script>
-<script src="js/jquery-ui.min.js"></script>
-<script src="js/jquery.fileupload.js"></script>
-<link href="css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-<link id="bs-css" href="css/bootstrap-cerulean.css" rel="stylesheet">
+    require_once("_inc.php");
+    require_once("./include/_function.php");
 
-<link rel="stylesheet" href="css/jquery.fileupload.css">
-<link rel="stylesheet" href="css/jquery.fileupload-ui.css">
-<noscript>
-    <link rel="stylesheet" href="css/jquery.fileupload-noscript.css">
-</noscript>
-<noscript>
-    <link rel="stylesheet" href="css/jquery.fileupload-ui-noscript.css">
-</noscript>
+    // 新增
+    if($_REQUEST["st"] == "ups"){
+        $an = SqlFilter($_REQUEST["an"],"int");
+        $d1 = SqlFilter($_REQUEST["d1"],"tab");
+        if($an == "" || $d1 == ""){
+            exit();
+        }
 
+        $SQL = "update webdata set d1='".$d1."' where auton=".$an." and types='actiondownload'";
+        $rs = $SPConn->prepare($SQL);
+        $rs->execute();
+        exit();  
+    }
+
+    // 上傳圖檔跟刪除圖檔(待測)
+    if($_REQUEST["st"] == "upload"){
+        $an = SqlFilter($_REQUEST["an"],"int");
+        if($an == ""){
+            $an = 0;
+        }
+
+        $SQL = "SELECT * FROM webdata where auton=".$an." and types='actiondownload'";
+        $rs = $SPConn->prepare($SQL);
+        $rs->execute();
+        $result = $rs->fetch(PDO::FETCH_ASSOC);
+        if($result){
+            $old_photo_name = $result["d2"];
+            if($old_photo_name != ""){
+                DelFile("upload_image/".$old_photo_name); //刪除圖檔
+            }
+        }
+
+        if ($_FILES['fileupload']['error'] === UPLOAD_ERR_OK){
+            $urlpath = "upload_image/"; //儲存路徑
+            $ext = pathinfo($_FILES["fileupload"]["name"], PATHINFO_EXTENSION); //附檔名      
+            $fileName = "event_".$an.".".$ext; //檔名
+            move_uploaded_file($_FILES["fileupload"]["tmp_name"],($urlpath.$fileName)); //儲存檔案
+            // 更新連結位置跟圖檔名稱
+            $SQL = "update webdata set d2='".$fileName."' where auton=".$an."";
+            $rs = $SPConn->prepare($SQL);
+            $rs->execute();
+            echo $an;
+            exit();
+        }
+    }
+
+    $img = "../img/lovepy_noimg.jpg";
+    if($_REQUEST["an"] != ""){
+        $SQL = "SELECT * FROM webdata where auton=".SqlFilter($_REQUEST["an"],"int")." and types='actiondownload'";
+        $rs = $SPConn->prepare($SQL);
+        $rs->execute();
+        $result = $rs->fetch(PDO::FETCH_ASSOC);
+        if($result){
+            $d1 = $result["d1"];
+            $img = $result["d2"];
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>春天網站系統</title>
+    <script src="js/jquery-1.8.3.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.fileupload.js"></script>
+    <link href="css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
+    <link id="bs-css" href="css/bootstrap-cerulean.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/jquery.fileupload.css">
+    <link rel="stylesheet" href="css/jquery.fileupload-ui.css">
+    <noscript>
+        <link rel="stylesheet" href="css/jquery.fileupload-noscript.css">
+    </noscript>
+    <noscript>
+        <link rel="stylesheet" href="css/jquery.fileupload-ui-noscript.css">
+    </noscript>
+</head>
+<body>
+    
 <form method="post" action="springweb_fun24_add.php?st=edit">
     <p>
-        <label>新竹 活動表下載圖檔：</label>
+        <label><?php echo $d1; ?> 活動表下載圖檔：</label>
     <dl>
         <div>
             <span class="btn btn-info fileinput-button"><span>選擇檔案</span><input data-no-uniform="true" id="fileupload" type="file" class="fileupload" name="fileupload"></span>
@@ -28,7 +106,7 @@
     </dl>
     </p>
 
-    <p>event_441.jpg<br><img height=80 src="upload_image/event_441.jpg?t=3184" id="showimg"></p>
+    <p><?php echo $img; ?><br><img height=80 src="upload_image/<?php echo $img; ?>?t=<?php echo rand(1,9999); ?>" id="showimg"></p>
 
     </div>
     <center><button type="submit" class="btn btn-danger" onclick="window.close()" style="width:40%;height:32px;">關閉</button></center>
@@ -44,7 +122,7 @@
             var $imgs = $(this).closest("span").find("#cimg").val();
 
             $this.fileupload({
-                    url: "springweb_fun24_add.php?st=upload&an=441",
+                    url: "springweb_fun24_add.php?st=upload&an=<?php echo SqlFilter($_REQUEST["an"],"int"); ?>",
                     type: "POST",
                     dropZone: $this,
                     dataType: 'html',
@@ -90,3 +168,5 @@
 
     });
 </script>
+</body>
+</html>
